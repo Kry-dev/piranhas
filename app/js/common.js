c1a65b11c2d030aea02f237059ae9e44bda7d5dd
kry-dev <yaroslavkryvda@gmail.com>
2019-03-14 06:14:18 +0200

    add map/ add private-public social js

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,17 @@
 $(function() {
+    // Custom JS
+    $('.places-private > a').on('click', function(e) {
+        e.preventDefault();
+        $('.places-public').removeClass('active');
+        $(this).addClass('active');
+        $('.social').hide();
+    });
 
-	// Custom JS
+    $('.places-public > a').on('click', function(e) {
+        e.preventDefault();
+        $('.places-private').removeClass('active');
+        $(this).addClass('active');
+        $('.social').show();
+    });
 
 });
